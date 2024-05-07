e6ee647a71af951f02157e8723c8e8f5c64ef3f7
Andrew Wijaya <andrew.a.wijaya@gmail.com>
2024-05-07 17:45:16 +0100

    Initial Commit

---
+++ MyDaylist/MyDaylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDaylistApp.swift
+//  MyDaylist
+//
+//  Created by Andrew Wijaya on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyDaylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
